8a5b1f2b4e45ca1de470462bafe66a069821b5dc
Ananda-Putra <balqisananda24@gmail.com>
2026-03-04 14:10:45 +0700

    ok

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        return view('admin');
+    }
+
+    public function user()
+    {
+        return view('user');
+    }
+}
